03a10e71bc4b97cfb8a525654ccb2551e5c32b84
kpeenee <43548093+kpeenee@users.noreply.github.com>
2018-10-24 10:41:56 +0100

    Added Movement

---
+++ Zombie Shooter/Assets/Scripts/TopDownController2D.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TopDownController2D : MonoBehaviour
+{
+    public float speed = 5.0f;
+    Rigidbody2D rb;
+
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+
+    void FixedUpdate()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+
+
+        rb.velocity = new Vector2(x, y) * speed;
+        rb.angularVelocity = 0.0f;
+    }
+}
+
+       
+    
