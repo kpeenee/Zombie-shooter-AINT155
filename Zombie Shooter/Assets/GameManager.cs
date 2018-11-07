687ae126eb09daaaa60aeceaba0a616a27b530e6
kpeenee <43548093+kpeenee@users.noreply.github.com>
2018-11-07 11:56:40 +0000

    Menu

---
+++ Zombie Shooter/Assets/GameManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+public class GameManager : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Zombie shooter level 1");
+    }
+    public void EndGame()
+    {
+        SceneManager.LoadScene("Game Over");
+    }
+    public void BackToMainMenu()
+    {
+        SceneManager.LoadScene("Main Menu");
+    }
+}
